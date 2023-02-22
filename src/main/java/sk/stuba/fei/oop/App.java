9bc179fe917616b1214f238c1809d2134224669b
JozefGulas <xgulas@stuba.sk>
2023-02-22 11:29:38 +0100

    initial commit

--- src/main/java/sk/stuba/fei/oop/App.java
+++ src/main/java/sk/stuba/fei/oop/App.java
@@ -1,7 +1,22 @@
 package sk.stuba.fei.oop;
 
 public class App {
+
+    static int compare(int firstNum, int secondNum){
+        if (firstNum > secondNum){
+            return 1;
+        }
+        else if (firstNum == secondNum){
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("start");
+        System.out.println("Hello world");
+        int result = compare(4, 5);
+        System.out.println(result);
     }
 }
